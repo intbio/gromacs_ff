8dab4ba2b557e70657f3ead21065d3e306c1c49f
Alexey Shaytan <alex@intbio.org>
2020-01-15 01:36:55 +0300

    Update

--- charmm36-mar2019_cufix.ff/convert_cufix.py
+++ charmm36-mar2019_cufix.ff/convert_cufix.py
@@ -5,4 +5,4 @@ with open('toppar_water_ions_cufix_simpl.str','r') as f:
 		else:
 			a=l.split()
 			if(len(a)>3):
-				print('%s %s 1 %f %f'%(a[0],a[1],0.1*float(a[3])/(2**(1/6)),float(a[2])*4.184))
+				print('%s %s 1 %f %f'%(a[0],a[1],0.1*float(a[3])/(2**(1/6)),-float(a[2])*4.184))
